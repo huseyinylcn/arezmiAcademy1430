203f150215ecd92d290e5208bbb528b8ffe129b9
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-10-16 21:29:41 +0300

    ders 2
    BU derste python veri türlerini ve dönüşümlerini ve temel 4 operatörü gördük

---
+++ lesson_1.py
@@ -0,0 +1,121 @@
+
+# Burada adımı name değişkeninene atadım
+# name = 'Hüseyin'
+# surname = 'Yalçın'
+# age = '23'
+
+# text = """ 
+# name: Hüseyin
+# surname: Yalçın
+# """
+
+# print(name, surname, age,text)
+
+
+# name = 'Hüseyin'
+# print(name)
+# name = 'Fatma'
+# print(name)
+
+
+#  str
+# name = 'Ali'
+
+# # int
+# age = 23
+
+# # float
+# no = 23.5
+
+# tipi1 = type(name)
+# tipi2 = type(age)
+# tipi3 = type(no)
+
+
+# print(tipi1,tipi2,tipi3)
+
+
+# txt = 'Konya\'nın Emre\'nin'
+# print(txt)
+
+
+# Bölme
+# print(10 / 3)
+
+# # çarpma
+# print(2*2)
+
+# # toplama
+# print(2+2)
+
+# # cıkarma
+# print(10 - 4)
+
+
+# sayi1 = 20
+# sayi2 = 30
+
+# toplam = sayi1 + sayi2
+# carpma = sayi1 * sayi2
+# cikarma = sayi1 - sayi2
+# bolme = sayi1/sayi2
+
+# print("toplamı", toplam)
+# print("carpımı", carpma)
+# print("cıkarma", cikarma)
+# print("bölme", bolme)
+
+
+
+
+
+# str ile int toplanmaz str ile str sadece toplanır
+
+# name = 'Hüseyin'
+# age = "23"
+
+# txt = 'Benim' + ' ' + 'adım '+ ' ' + name + ' ' + 'yaşım' + ' ' + age
+
+# print(txt)
+
+
+
+
+
+
+
+# age = 23
+
+# txt = 'yaşım '+ str(age)
+
+# print(txt) 
+
+
+
+
+
+
+
+
+# age = input("yaşını gir")
+
+# print("yaşınız ", age)
+
+# year = 2024
+
+# birth = input('doğum tarihini gir : ')
+
+# age = year - int(birth)
+
+# print("yaşınız ",age)
+
+
+
+# limit = input("kredi kartı limitiniz : ")
+
+# limit = int(limit)
+
+# if limit > 100000:
+#     print("750tl vergi ödeyeceksiniz ")
+# else:
+#     print("vergi ödemeyeceksiniz")
