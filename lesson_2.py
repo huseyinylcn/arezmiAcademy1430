e02977c766b4f4ef89cdcd4d8beec55c5c0cd1a0
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-10-18 20:54:01 +0300

    ders 3

---
+++ lesson_2.py
@@ -0,0 +1,145 @@
+# sayi1 = input("toplamak istediğiniz 1. sayıyı girin: ")
+# sayi2 = input("toplamak istediğiniz 2. sayıyıy girin: ")
+
+# sayi1 = float(sayi1)
+# sayi2 = float(sayi2)
+
+# toplm = sayi1 + sayi2
+
+# print("sonuc: ")
+# print(toplm)
+
+
+# # + - * /
+# kalanı verir
+# print(13%11)
+
+# # üst alma
+# print(2**4)
+
+# # kalansız bölme
+# print(13//11)
+
+# sayi = 12
+
+# # toplama ve atama operatörü
+# sayi = sayi + 5
+# sayi += 5
+
+# # carpma ve atama operatörü
+# sayi = sayi * 5
+# sayi *= 5
+
+# # bölme ve atama operatörü
+# sayi = sayi / 5
+# sayi /= 5
+
+# # çıkarma ve atama operatörü
+# sayi = sayi - 5
+# sayi -= 5
+
+# # kalanı bulma ve atama operatörü
+# sayi = sayi % 5
+# sayi %= 5
+
+# # kalansız bölme ve atama operatörü
+# sayi = sayi // 5
+# sayi //= 5 
+
+# # üst alma ve atama operatörü
+# sayi = sayi**3
+# sayi **= 3
+
+
+# print(sayi)
+
+
+
+# True
+# False
+
+
+# print(type(True))
+
+
+# sonuc = False
+# tip = type(sonuc)
+
+# print(tip)
+
+
+
+# print( 2 == 2)
+
+# print( 4 != 2)
+
+# print(3 > 5)
+
+# print(3 < 5)
+
+# print(4 <= 5)
+
+
+# x = 2 >= 4
+# print(type(x))
+
+# tahmin = "5"
+
+# tahminEdilenSayi = input("tahmin edilen sayıyı bul : ")
+
+# print(tahmin == tahminEdilenSayi)
+
+
+# password = "12345678"
+
+# inputPassword = input("Şifreni gir: ")
+
+# print("şifren :", password == inputPassword)
+
+
+
+
+# kullanıcıdan 2 adet not al ve bu notların ortalamasını alıp konsola yazdır
+
+
+
+
+# kullanıcıda vize ve final olmak üzere 2 adet not alın vizenin %40 finalin %60 alın ve notu terminale yazdırın
+
+
+# vize = input('vize notunuzu girin : ')
+# final = input('final notunuzu girin : ')
+
+# vize = float(vize)
+# final = float(final)
+
+# print('Notunuz : ', (vize*0.4) + (final*0.6) )
+
+
+
+# password = '123'
+# username = 'hsyn'
+
+# inputpassword =  input("şifre: ")
+# inputusername = input("username: ")
+
+# sonuc = password == inputpassword and username == inputusername
+
+# print(sonuc)
+
+
+# password = '123'
+# username = 'hsyn'
+
+# inputpassword =  input("şifre: ")
+# inputusername = input("username: ")
+
+# sonuc = password == inputpassword or username == inputusername
+
+# print(sonuc)
+
+# print(not False)
+
+
+
+
