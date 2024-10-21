719d744c6c1cce3c215777aefdfa6ba822c8a4b6
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-10-21 20:46:41 +0300

    ders 4

---
+++ lesson_3.py
@@ -0,0 +1,155 @@
+# txt = "Python derslerine Hoşgeldiniz"
+
+# # upper() metodu tüm harfleri büyük yapar
+# txt = txt.upper()
+
+# print(txt)
+
+
+# txt = "Python Derslerine hoşgeldiniz"
+
+# # lower metodu tüm harfleri küçük yapar
+# txt = txt.lower()
+
+# print(txt)
+
+
+
+# txt = "Python derslerine hoşgeldiniz"
+
+# # title metodu tüm baş harfleri büyük yapar
+# txt = txt.title()
+
+# print(txt)
+
+
+
+# txt = "python dErslerine hoşgeldiniz"
+
+# # capitalize tüm harfleri küçük yapar sadece başharfi büyük yapar
+# txt = txt.capitalize()
+
+# print(txt)
+
+
+# txt = "python dErslerine hoşgeldiniz"
+
+# # casefold tüm harfleri küçük yapar lower metoduna göre daha agresiftir
+# txt = txt.casefold()
+
+# print(txt)
+
+
+
+# txt = "python dErslerine hoşgeldiniz"
+
+# kesit = txt[ 5: ]
+
+# print(kesit.upper())
+
+
+
+# elinizdeki metinin 5 karakterden 15 karakterlerini alın ve aldığınız yeni metnin tüm harflerini büyük yaparak konsola yazdırın
+
+
+
+# name = 'Hüseyin'
+
+# name = name.center(15,"*")
+# print(len(name))
+
+
+
+# txt = "python dErslerine hoşgeldiniz"
+
+# sonuc = txt.count("python")
+
+# print(sonuc)
+
+
+
+# txt = "python dErslerine hoşgeldiniz"
+
+# aranan = input("aranacak kelime: ")
+
+# snc = txt.count(aranan)
+
+# print("sonuc", snc)
+
+
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# sonuc = txt.count("hoş")
+
+# print(sonuc)
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# snc = txt.find("dsfsdf")
+
+# print(snc)
+
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# snc = txt.index("hoş")
+
+# print(snc)
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# aranan = input("aranan kelime : ")
+
+# print(txt.index(aranan))
+
+
+# name = "Hüseyin"
+
+# print(name[  :-6 ])
+
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+
+# snc = txt.endswith(" hoş geldiniz")
+
+# print(snc)
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# snc = txt.startswith("asdsadas")
+
+# print(snc)
+
+
+# txt = "        python dErslerine hoş geldiniz hoş geldiniz       "
+
+# print(txt.strip())
+
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+
+# txt = txt.replace("python","sadasd dsadas")
+
+# print(txt)
+
+# txt = "python dErslerine hoş geldiniz hoş geldiniz"
+# print(txt)
+
+# change = input("yukarıdaki metinde değiştirmek istediğiniz yeri girin: ")
+# change2 = input("ne ile değiştirmek istiyorsunuz : ")
+
+# txt = txt.replace(change,change2)
+
+# print(" değişen öetniniz yandadır. ",  txt)
+
+
+
+# x = 'Aasdas'
+
+# print(x.istitle())
+
+
+
+
