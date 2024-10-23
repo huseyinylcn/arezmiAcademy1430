18939417b7f8a00534e275963af754eeb1f214d7
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-10-23 21:09:43 +0300

    ders 5

---
+++ lesson_4.py
@@ -0,0 +1,116 @@
+# if else elif print(sep, end, file)  open("database.txt","a")
+
+
+
+
+
+
+
+
+# dert metre
+
+# print("""
+#       Derdinizi girin 
+#       1 büyük 
+#       0 küçük
+#       """)
+
+# inp = input("Dersinizi Girin :")
+
+# if inp == "1":
+#     print("derdiniz Büyük")
+# else:
+#     print("derdiniz Küçük")
+
+
+
+# age = input("lütfen yaşınızı giriniz : ")
+
+# age = int(age)
+
+# if age >= 18 and age < 30 :
+#     print("yetişkin")
+# elif age >= 30 and age < 50:
+#     print("olgunusun")
+# elif age >= 50:
+#     print("yaşlısınız")
+# else:
+#     print("reşit değil")
+
+
+
+# notum = input("Notunuzu girn : ")
+
+# notum = float(notum)
+
+# if notum >= 90:
+#     print("AA aldınız")
+    
+# elif notum >= 80:
+#     print("AB aldınız")
+    
+# elif notum >=70 :
+#     print("BB oldu")
+    
+# else:
+#     print("kaldınız")
+
+
+# dbusername = "hsyn"
+# dbpassword = "123"
+
+# username = input("kullanıcı adınızı girin :")
+
+
+# if dbusername == username:
+#     print("kullanı adınız doğru")
+#     password = input( "şifrenizi girin : ")
+#     if password == dbpassword:
+#         print("şifreniz doğru sisteme hoşgeldiniz")
+#     else:
+#         print("şifreniz yanlış ")
+# else:
+#     print("kullanıcı adınız yanlış")
+#     print()
+#     x = input("yeni hesap oluşturmak için 1 girin istemiyorsanız 0 girin")
+#     if x == "1":
+#         print("kullanıcı yeni hesap oluşturmak istedi")
+#     elif x == "0":
+#         print("kullanıcı hesap oluşturmak istemedi")
+
+
+# name = "Hüseyin"
+
+# print("Merhaba dünya",name,"yyyy",sep="*",end="*")
+
+
+# db = open("deneme.txt","a")
+
+# print("password error",file=db)
+
+# db = open("deneme.txt","a")
+# print("name",end="*",sep="#",file=db)
+# db.close()
+
+# print( "hüseyin","yalçın",sep="*",end="*",file=db)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
