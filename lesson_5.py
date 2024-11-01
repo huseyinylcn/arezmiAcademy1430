31c6af369e24779cb3a28ce53c6a8cea015ee3f5
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-11-01 21:17:11 +0300

    ders 7

--- lesson_5.py
+++ lesson_5.py
@@ -52,37 +52,37 @@
 
 
 
-soru1 = input("evlimisin evliysen 1 değilsen 2 : ")
+# soru1 = input("evlimisin evliysen 1 değilsen 2 : ")
 
-if soru1 == "1":
-    soru2 = input("çocuğun varmı varsa 1 yoksa 2 ")
-    if soru2 == "1":
-        soru3 = input("kac çocuğun var: ")
-        if soru3.isdigit():
-            soru3 = int(soru3)
-            if soru3 >= 3:
-                print("cumhurbaşkanını  sözünü dinlemişsin")
-            elif soru3 == 1:
-                soru4 = input("kaç yılında doğdu: ")
-                if soru4.isdigit():
-                    soru4 = int(soru4)
-                    age = 2024 - soru4
-                    if age < 18:
-                        print("18 yaşından küçük bir cocuğun var")
-                    elif age >= 18 and age < 30:
-                        print("genc bir çocuğun var")
-                    elif age >= 30 and age < 50:
-                        print("olgun bir oğlun var sen yaşlı olmalısın")
-                    else:
-                        print("yaşlı bir oğlun var")
-                else:
-                    print("çocuğunun yaşını sayı olarak gir. ")
-            else:
-                print("2 cocuk iyidir 3 cocuk daha iyidir")
-        else:
-            print("kaç cocuğun var sorusuna sayı gir. ")
-    else:
-        print("evlisin ama çocuğun yokmuş")
-else:
-    print("evli değilmiş")
+# if soru1 == "1":
+#     soru2 = input("çocuğun varmı varsa 1 yoksa 2 ")
+#     if soru2 == "1":
+#         soru3 = input("kac çocuğun var: ")
+#         if soru3.isdigit():
+#             soru3 = int(soru3)
+#             if soru3 >= 3:
+#                 print("cumhurbaşkanını  sözünü dinlemişsin")
+#             elif soru3 == 1:
+#                 soru4 = input("kaç yılında doğdu: ")
+#                 if soru4.isdigit():
+#                     soru4 = int(soru4)
+#                     age = 2024 - soru4
+#                     if age < 18:
+#                         print("18 yaşından küçük bir cocuğun var")
+#                     elif age >= 18 and age < 30:
+#                         print("genc bir çocuğun var")
+#                     elif age >= 30 and age < 50:
+#                         print("olgun bir oğlun var sen yaşlı olmalısın")
+#                     else:
+#                         print("yaşlı bir oğlun var")
+#                 else:
+#                     print("çocuğunun yaşını sayı olarak gir. ")
+#             else:
+#                 print("2 cocuk iyidir 3 cocuk daha iyidir")
+#         else:
+#             print("kaç cocuğun var sorusuna sayı gir. ")
+#     else:
+#         print("evlisin ama çocuğun yokmuş")
+# else:
+#     print("evli değilmiş")
     
---
+++ lesson_6.py
@@ -0,0 +1,183 @@
+# meyve = ["elma","armut","kiraz"]
+# len, type
+# print(meyve[0])
+
+# meyve.append("şeftali")
+# print(meyve)
+
+# meyve.insert(7,"karpuz")
+# print(meyve)
+
+
+# meyve.remove('karpuz')
+# print(meyve)
+
+
+# meyve.pop()
+
+# meyve.pop(2)
+
+# meyve.sort(reverse=True)
+
+# meyve.clear()
+# del meyve[0]
+
+# print(meyve)
+
+# meyve2 = meyve.copy()
+
+# print(meyve)
+# print(meyve2)
+
+# meyve2.pop()
+# print(meyve)
+# print(meyve2)
+
+
+# meyve = ["elma","armut","erik","kiraz"]
+# print(meyve)
+# print(len(meyve))
+# print(meyve[2])
+# print( type(meyve) )
+
+# print(meyve)
+# # meyve.append("karpuz")
+# meyve.insert(2,"şeftali")
+# print(meyve)
+
+# meyve.remove("elmas")
+# meyve.pop(2)
+# del meyve[2]
+
+# meyve.append("ceviz")
+# meyve.pop(1)
+# # print(12)
+# # burada meyveleri konsola yazdırdım
+# print(meyve)
+
+
+
+
+# meyve = ["elma","armut","erik","kiraz"]
+
+# # sort verilerimiz sıralar reverse true olurasa terini sıralar
+# # meyve.sort(reverse=False)
+# # reverse ters cevirir
+# # meyve.reverse()
+# meyve2 = meyve.copy()
+
+# print("meyve listesi", meyve)
+# print("meyve2 listesi", meyve2)
+
+
+# meyve2.remove("elma")
+
+# print("meyve listesi", meyve)
+# print("meyve2 listesi", meyve2)
+
+
+# meyve = ["elma","armut","erik","kiraz"]
+
+
+# meyve.sort(reverse=True)
+# meyve.reverse()
+
+# meyve2 = meyve.copy()
+# print(meyve)
+# meyve2.append( 12 )
+
+# print(meyve2)
+
+
+# birth = ( 1995, 1975, 2001, 2002, 2005 )
+
+# print(type(birth))
+
+# birth = list(birth)
+
+# birth.append(1946)
+
+# print(birth)
+
+# print(type(birth))
+
+# birth = tuple(birth)
+
+# print(type(birth))
+
+# print(birth)
+
+# birth = list(birth)
+
+
+# name_array = ( "Hüseyin", "Hamit","Elif","Ebru","Nisa" )
+
+# print(type(name_array))
+# print(len(name_array))
+# print(name_array[0:3])
+
+# name_array = list(name_array)
+
+# name_array.append("Raziye")
+
+# print(name_array)
+
+# name_array = tuple(name_array)
+
+
+# my_array = [12,False,"Arazmi Akdami", 12.6]
+
+# my_array[0] = my_array[0]  + 5
+
+# my_array[2] = my_array[2].upper()
+
+# my_array[-1] = int(my_array[-1])
+
+# my_array[1] = not(my_array[1])
+
+# print(type(my_array[2]))
+
+# print( my_array)
+
+# if my_array[1]:
+#     print("ifade doğruymuş")
+# else:
+#     print("ifade yanlışmış")
+
+
+# myArray = ["elma","armut","kiraz"]
+
+# print(myArray)
+
+# inp = input("silmek istediğiniz ifadeyi yazın silmek istemiyorsanız quit yazın: ")
+
+# if inp != 'quit':
+#     myArray.remove(inp)
+#     print("veriniz silindi..")
+#     print("Güncel veri",myArray)
+# else:
+#     print("çıkış yapılıyor")
+
+
+
+# myArray = ("elma","armut","kiraz")
+
+# print(myArray)
+
+# inp = input("silmek istediğiniz ifadeyi yazın silmek istemiyorsanız quit yazın: ")
+
+# if inp != 'quit':
+#     myArray = list(myArray)
+#     myArray.remove(inp)
+#     print("veriniz silindi..")
+#     print("Güncel veri",myArray)
+#     myArray = tuple(myArray)
+# else:
+#     print("çıkış yapılıyor")
+
+
+
+
+
+
+
