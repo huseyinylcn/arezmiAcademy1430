50076f1d3f218876e988ebf9542c3a30cea5885e
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-11-04 23:54:59 +0300

    again

---
+++ again.py
@@ -0,0 +1,202 @@
+# Print komutu terminalden çıktı almamızı sağlar
+# print('Hüseyin Yalçın')
+# print(12)
+# print(12.4)
+
+
+# int, veri türüdür ve bu tam sayıları temsil edir 
+# print( 12 )
+
+# str veri türüdür ve bu metinleri temsil eder
+# "Python dersleri"
+# ' tek tırnak '
+
+# print( "Hüseyin Yalçın " )
+
+# float
+# 12.4 
+# 11.0
+
+# print( 23.5 )
+
+# terminale int , str ve float tipinde veriler yazdırın 
+
+# print( 12 )
+# print( "arezmi ile öğren" )
+# print( 12.4 )
+
+
+# type( 12 ) komutu bana verimin tipini verir 
+
+# type(12.4) 
+
+
+# print( type("12.5") )
+
+
+
+# print( type(" ben metinim ") )
+# print( type(12) )
+# print( type(3.5) )
+
+
+
+# = operatörü atama operatörüdür
+
+
+# metin = " Arezmi academy ders tekrarları "
+# sayi = 12
+# sayi2 = 12.3
+
+
+# print( sayi )
+# print( metin )
+# print( sayi2 )
+
+# değişken isimleri sayı ile başlayamaz ve içerisnde ! ? { } vb karakterler bulunduramaz 
+
+
+
+# temel operatörler 
+
+# = atama operatörü
+
+# + toplama operatörü 
+
+#  - çıkarma operatörü
+
+#  * çarpma operatörü 
+
+# / bölme operatörü
+
+# ** üst alma operatörü
+
+#  // mod alma 
+
+
+# result1 = 3 // 6
+# print(result1)
+
+# result2 = 5**2
+# print(result2)
+
+# result3 = 5 - 3
+# print(result3)
+
+# result4 = 2 + 2
+# print(result4)
+
+# result5 = 3 / 2
+# print(result5)
+
+# result6 = 3 * 4
+# print(result6)
+
+
+
+
+#^ str ile int veri türleri toplnamaz veya çıkartılamaz  
+#! str ile float veri türleri toplanamz çıkarılaz
+#TODO int ve float veri türleri toplanabilir veya çıkarılabilir vb.
+#~ str ve str veri türleri sadece toplanabilir. 
+
+
+# result = "12" + " ahmet"
+# print(result)
+
+
+
+# result = "Ahmet"+ " "+ "Amca" + " " + "Nasılsın"
+
+# print(result)
+
+# name = "Hüseyin"
+# surname = 'Yalçın'
+
+#^ print komutuna birden fazla ifade girmek istersek virgül ile ayırabiliriz 
+# print( name, surname, 12 ,"Arezmi" )
+
+#TODO 3 adet değişken tanımlayın ve bunları tek bir print içierinde konsola yazdırın süre 2 dk
+
+# name = 'Hüseyin'
+# surname = 'Yalçın'
+# city = 'Edirne'
+
+# print(name, surname, city)
+
+
+
+#& değişkenlerimi sonrada güncellyebilirz atama operatörü ile
+# age = 12
+# print(type(age))
+# age = "12"
+# print(type(age))
+
+#~ String İfadeleri integer çevirme  
+
+
+
+
+
+# sayi = '12'
+# print("ilk tipi", type(sayi) )
+
+# sayi = int(sayi)
+
+# result = 13 + sayi
+# print("son tipi", type(sayi))
+
+# print(result)
+
+# sayi1 = "37"
+
+# sayi1 = int(sayi1)
+
+# result = sayi1 + 5.5
+# print(result)
+
+
+#! String bir sayi oluşturun ve bir değişkene atayın daha sonra bu verinin tipini konsola yazdırın süre 1.5dk
+
+
+
+# ali  = "212"
+# mehmet = "312"
+
+# ali = int(ali)
+# mehmet = int(mehmet)
+
+# print( "alinin tipi ",type(ali) )
+
+# print( "mehemetin tipi ",type(mehmet) )
+
+# print("sonuc",  ali + mehmet)
+
+
+
+# konya = "12"
+# manisa = "144"
+
+# konya = int(konya)
+# manisa = int(manisa)
+
+# print( type(konya), "konyanın tipi" )
+# print( type(manisa) , "manisanın tipi" )
+
+# toplam = konya + manisa
+
+# print( "sonuc", toplam  )
+
+
+#~ int ifadeleri str cevirme
+
+
+# ahmet = 2010
+# ahmet = str(ahmet)
+# metin = "ahmet " + ahmet + " yıl " + "okula başladı"
+# print(metin)
+
+
+
+# https://www.w3schools.com/python/default.asp
+
